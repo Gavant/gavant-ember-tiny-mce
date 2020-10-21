677df6ad106fdb0f85fd5c13adc27a1ccd503dbe
Nick Pafundi <npafundi@gavant.com>
2020-10-21 16:38:20 -0400

    setup repo

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+// Types for compiled templates
+declare module 'gavant-ember-tiny-mce/templates/*' {
+  import { TemplateFactory } from 'htmlbars-inline-precompile';
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
