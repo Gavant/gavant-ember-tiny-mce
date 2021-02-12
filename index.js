5ba1c87abf2c5aa8ab5d730f7d920c5ed8c5f889
Adam Baker <bakerac4@gmail.com>
2021-02-12 10:00:30 -0500

    More cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const Funnel = require('broccoli-funnel');
 module.exports = {
-    name: '@gavant/ember-tiny-mce',
+    name: '@gavant/ember-tinymce',
 
     treeForPublic() {
         const tinymceTree = new Funnel('node_modules/tinymce/', {
